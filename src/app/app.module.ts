c9c52dcc67af5579dcbf1dcaf88e8abf0b2c075a
Somnath Saha <saheb4u@gmail.com>
2023-11-27 12:51:01 +0530

    Initial Checkin 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './Counter/State/counter.reducer';
+import { CounterButtonComponent } from './Counter/counter-button/counter-button.component';
+import { CounterOutputComponent } from './Counter/counter-output/counter-output.component';
+import { CounterComponent } from './Counter/counter/counter.component';
+import { CustomInputCounterComponent } from './Counter/custom-input-counter/custom-input-counter.component';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './shared/component/header/header.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent,
+    CounterOutputComponent,
+    CounterButtonComponent,
+    CustomInputCounterComponent,
+    HomeComponent,
+    HeaderComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    StoreModule.forRoot({ count: counterReducer }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
